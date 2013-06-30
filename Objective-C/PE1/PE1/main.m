1ac18a6648995115ca8122f766f964bcb63001a0
Matthew Chan <mattkc7@gmail.com>
2013-06-30 04:32:23 -0700

    Correct directory placement

--- Objective-C/PE1/PE1/main.m
+++ Objective-C/PE1/PE1/main.m
@@ -12,12 +12,11 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
         /*
-         If we list all the natural numbers below 10 that are multiples of 3 or 5, 
-         we get 3, 5, 6 and 9. The sum of these multiples is 23.
-         
-         Find the sum of all the multiples of 3 or 5 below 1000.
+             If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+             we get 3, 5, 6 and 9. The sum of these multiples is 23.
+             
+             Find the sum of all the multiples of 3 or 5 below 1000.
          */
     
         int sumSoFar = 0;
