f3594b1c0807fa6da0a0a85fd31c2f1030d92259
Matthew Chan <mattkc7@gmail.com>
2013-07-13 19:33:07 -0700

    Sum of all primes less than 2 mil is now computed in about 4 seconds via Sieve of Eratosthenes

--- Ruby/PE10.rb
+++ Ruby/PE10.rb
@@ -6,52 +6,92 @@ Find the sum of all the primes below two million.
 
 =end
 
+################################
+########    Attempt 2   ########
+################################
+capacity = 2000000
+primes_number_range = Array.new(capacity)
+puts primes_number_range.size
 
-def is_Prime?(num)
-  for i in $primes_So_Far
-    if (num % i) == 0
-        return false
-    end
-  end 
-  # limit = num / 2
-  #   for i in (2..limit)
-  #     if (num % i) == 0
-  #       return false
-  #     end
-  #   end
-  
-  return true
+for i in (0..capacity)
+  primes_number_range[i] = 1
 end
 
-def next_prime_after(num)
-  potential_prime = num + 1
-  keep_searching = true
-  while keep_searching
-    if is_Prime?(potential_prime)
-      keep_searching = false
-      return potential_prime
-    end
-    
-    potential_prime += 1
+primes_number_range[0] = 0
+primes_number_range[1] = 0
+primes_number_range[capacity] = 0
+
+curr_prime = 2
+
+while ((curr_prime * curr_prime) < capacity)
+  k = 2
+
+  while (curr_prime * k < capacity)
+    primes_number_range[curr_prime * k] = 0
+    k += 1
+  end
+  
+  curr_prime += 1
+end
+
+sum = 0
+for i in (0..capacity)
+  if primes_number_range[i] == 1
+    sum += i
   end
 end
 
-def sum_of_all_primes_below_two_million
-  sum = 0
-  curr_prime = 2
-  $primes_So_Far = Array.new
-  
-  while curr_prime < 2000000
-    puts "curr_prime= #{curr_prime} | sum= #{sum}"
-    if is_Prime?(curr_prime)
-      sum += curr_prime
-      $primes_So_Far.push(curr_prime)
-      curr_prime = next_prime_after(curr_prime)
-    end
-  end
-  
-  puts "#{sum}" # ans: 142913828922 
-                # Time: 49 min on a Macbook (2.66 GHz Intel Core 2 Duo Processor)
-end
+puts "Sum of all primes less than 2,000,000: #{sum}"
 
-sum_of_all_primes_below_two_million
+################################
+########    Attempt 1   ########
+################################
+
+# def is_Prime?(num)
+#   for i in $primes_So_Far
+#     if (num % i) == 0
+#         return false
+#     end
+#   end 
+#   # limit = num / 2
+#   #   for i in (2..limit)
+#   #     if (num % i) == 0
+#   #       return false
+#   #     end
+#   #   end
+#   
+#   return true
+# end
+# 
+# def next_prime_after(num)
+#   potential_prime = num + 1
+#   keep_searching = true
+#   while keep_searching
+#     if is_Prime?(potential_prime)
+#       keep_searching = false
+#       return potential_prime
+#     end
+#     
+#     potential_prime += 1
+#   end
+# end
+# 
+# def sum_of_all_primes_below_two_million
+#   sum = 0
+#   curr_prime = 2
+#   $primes_So_Far = Array.new
+#   
+#   while curr_prime < 2000000
+#     puts "curr_prime= #{curr_prime} | sum= #{sum}"
+#     if is_Prime?(curr_prime)
+#       sum += curr_prime
+#       $primes_So_Far.push(curr_prime)
+#       curr_prime = next_prime_after(curr_prime)
+#     end
+#   end
+#   
+#   puts "#{sum}" # ans: 142913828922 
+#                 # Time: 49 min on a Macbook (2.66 GHz Intel Core 2 Duo Processor)
+# end
+# 
+# sum_of_all_primes_below_two_million
