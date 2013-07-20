1d4f6028d841a7c0cf4edb3764f38e073951d6f1
Matthew Chan <mattkc7@gmail.com>
2013-07-20 01:31:58 -0700

    Added code to time the task

--- Objective-C/PE10/PE10/main.m
+++ Objective-C/PE10/PE10/main.m
@@ -18,9 +18,15 @@ int main(int argc, const char * argv[])
          * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
          * Find the sum of all the primes below two million.
          */
-        
+        NSDate *start = [NSDate date];
+
         SumOfPrimesUnderTwoMillion *operator = [[SumOfPrimesUnderTwoMillion alloc] init];
         [operator beginCalculations];
+        
+        NSDate *end = [NSDate date];
+        NSTimeInterval timeDifference = [end timeIntervalSinceDate:start];
+        
+        NSLog(@"Completed in %.3f seconds", timeDifference);
     }
     return 0;
 }
